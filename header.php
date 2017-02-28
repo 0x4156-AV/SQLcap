f712f3af43ee54bf928d6f5757ec5d17eaac12b4
Aaron Vigal <admin@aaronvigal.com>
2017-02-27 19:02:15 -0600

    Update header.php
    Updated references to sqlmap after cloning into /var/www/html

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <title id="ttl">SQLCAP</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/sqlmap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/sqlmap/css/css.css">
+    <link rel="stylesheet" href="/SQLcap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/SQLcap/css/css.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="/sqlmap/js/bootstrap.min.js"></script>
-    <script src="/sqlmap/js/sqlmap.js"></script>
+    <script src="/SQLcap/js/bootstrap.min.js"></script>
+    <script src="/SQLcap/js/sqlmap.js"></script>
   </head>
   <body style="background-color=#191919;">
     <br />
